c4104e5d3f621d4fc0c09400028672e00983233b
Elena Parapounsky <eparapounsky@gmail.com>
2025-08-15 10:30:15 -0700

    Format with SQL language features

--- DDL.sql
+++ DDL.sql
@@ -1,171 +1,359 @@
+---------------------------------------------------------------
 -- Data Definition Queries for the Pokemon Management System --
--- Team members: Elena Parapounsky & Amy Xu  --
+---------------------------------------------------------------
+SET
+	FOREIGN_KEY_CHECKS = 0;
 
--- Citation for setting foreign key checks/autocommits:
--- Date: 10/28/2024
--- Based on recommendation to disable commits and foreign checks from CS340 - Project Step 2 Draft: Normalized Schema + DDL with Sample Data
--- Source URL: https://canvas.oregonstate.edu/courses/1976520/assignments/9783693?module_item_id=24719057
-
-SET FOREIGN_KEY_CHECKS=0;
-SET AUTOCOMMIT = 0;
+SET
+	AUTOCOMMIT = 0;
 
 -- =====================
 -- DEFINE TABLES
 -- =====================
-
--- Citation for the following function(s):
--- Date: 10/28/2024
--- Based on CREATE/INSERT table function from CS340 - Activity 1: Creating a Customer Object Table
--- Source URL: https://canvas.oregonstate.edu/courses/1976520/pages/activity-1-creating-a-customer-object-table?module_item_id=24719034
-
-CREATE OR REPLACE TABLE Pokemon (
-    pokeID int AUTO_INCREMENT UNIQUE NOT NULL,
-    pokeName varchar(255) NOT NULL,
-    isBaby boolean NOT NULL,
-    pokeRarity varchar(255) NOT NULL,
+CREATE
+OR REPLACE TABLE Pokemon (
+	pokeID int AUTO_INCREMENT UNIQUE NOT NULL,
+	pokeName varchar(255) NOT NULL,
+	isBaby boolean NOT NULL,
+	pokeRarity varchar(255) NOT NULL,
 	PRIMARY KEY (pokeID)
 );
 
-CREATE OR REPLACE TABLE Affiliations (
-    affiliationID int AUTO_INCREMENT UNIQUE NOT NULL,
-    affiliationType varchar(255) NOT NULL,
-    affiliationRank varchar(255) NOT NULL,
-    typeDescription varchar(255),
+CREATE
+OR REPLACE TABLE Affiliations (
+	affiliationID int AUTO_INCREMENT UNIQUE NOT NULL,
+	affiliationType varchar(255) NOT NULL,
+	affiliationRank varchar(255) NOT NULL,
+	typeDescription varchar(255),
 	PRIMARY KEY (affiliationID)
 );
 
-CREATE OR REPLACE TABLE People (
-    peopleID int AUTO_INCREMENT UNIQUE NOT NULL,
-    peopleName varchar(255) NOT NULL,
-    affiliationID int,
-	PRIMARY KEY (peopleID), 
-	FOREIGN KEY (affiliationID) REFERENCES Affiliations(affiliationID) ON DELETE SET NULL
+CREATE
+OR REPLACE TABLE People (
+	peopleID int AUTO_INCREMENT UNIQUE NOT NULL,
+	peopleName varchar(255) NOT NULL,
+	affiliationID int,
+	PRIMARY KEY (peopleID),
+	FOREIGN KEY (affiliationID) REFERENCES Affiliations(affiliationID) ON DELETE
+	SET
+		NULL
 );
 
-CREATE OR REPLACE TABLE Types (
-    typeID int AUTO_INCREMENT UNIQUE NOT NULL,
-    typeName varchar(255) NOT NULL,
-    weakAgainst varchar(255) NOT NULL, 
-	strongAgainst varchar(255) NOT NULL, 
+CREATE
+OR REPLACE TABLE Types (
+	typeID int AUTO_INCREMENT UNIQUE NOT NULL,
+	typeName varchar(255) NOT NULL,
+	weakAgainst varchar(255) NOT NULL,
+	strongAgainst varchar(255) NOT NULL,
 	PRIMARY KEY (typeID)
 );
 
-CREATE OR REPLACE TABLE Battles (
-    battleID int AUTO_INCREMENT UNIQUE NOT NULL,
-    battleDate date NOT NULL,
-    battleSetting varchar(255) NOT NULL,
+CREATE
+OR REPLACE TABLE Battles (
+	battleID int AUTO_INCREMENT UNIQUE NOT NULL,
+	battleDate date NOT NULL,
+	battleSetting varchar(255) NOT NULL,
 	PRIMARY KEY (battleID)
 );
 
-CREATE OR REPLACE TABLE Pokemon_People (
-	pokeID int NOT NULL, 
-	peopleID int NOT NULL, 
-	pokeNickname varchar(255), 
-	caughtDate date NOT NULL, 
+CREATE
+OR REPLACE TABLE Pokemon_People (
+	pokeID int NOT NULL,
+	peopleID int NOT NULL,
+	pokeNickname varchar(255),
+	caughtDate date NOT NULL,
 	caughtAt varchar(255) NOT NULL,
 	PRIMARY KEY (pokeID, peopleID),
 	FOREIGN KEY (pokeID) REFERENCES Pokemon(pokeID) ON DELETE CASCADE ON UPDATE CASCADE,
 	FOREIGN KEY (peopleID) REFERENCES People(peopleID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE OR REPLACE TABLE Pokemon_Types (
-	pokeID int NOT NULL, 
-	typeID int NOT NULL, 
+CREATE
+OR REPLACE TABLE Pokemon_Types (
+	pokeID int NOT NULL,
+	typeID int NOT NULL,
 	PRIMARY KEY (pokeID, typeID),
 	FOREIGN KEY (pokeID) REFERENCES Pokemon(pokeID) ON DELETE CASCADE ON UPDATE CASCADE,
 	FOREIGN KEY (typeID) REFERENCES Types(typeID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE OR REPLACE TABLE People_Battles (
+CREATE
+OR REPLACE TABLE People_Battles (
 	peopleBattleID INT AUTO_INCREMENT UNIQUE NOT NULL,
-	peopleID int, 
-	battleID int NOT NULL, 
-	battleResult varchar(255) NOT NULL, 
-	UNIQUE (peopleID, battleID), -- make sure the same person isn't in the same battle more than once
+	peopleID int,
+	battleID int NOT NULL,
+	battleResult varchar(255) NOT NULL,
+	UNIQUE (peopleID, battleID),
+	-- make sure the same person isn't in the same battle more than once
 	PRIMARY KEY (peopleBattleID),
-	FOREIGN KEY (peopleID) REFERENCES People(peopleID) ON DELETE SET NULL ON UPDATE CASCADE, -- battles can remain even if a person is deleted
-	FOREIGN KEY (battleID) REFERENCES Battles(battleID) ON DELETE CASCADE ON UPDATE CASCADE -- if a battle is deleted, the associated people should also be deleted from this table
+	FOREIGN KEY (peopleID) REFERENCES People(peopleID) ON DELETE
+	SET
+		NULL ON UPDATE CASCADE,
+		-- battles can remain even if a person is deleted
+		FOREIGN KEY (battleID) REFERENCES Battles(battleID) ON DELETE CASCADE ON UPDATE CASCADE -- if a battle is deleted, the associated people should also be deleted from this table
 );
 
 -- =====================
 -- INSERT SAMPLE DATA 
 -- =====================
-
--- Citation for the following function(s):
--- Date: 10/24/2024
--- Pokemon data based on Pokemon website Pokedex
--- Source URL: https://www.pokemon.com/us/pokedex
--- Trainer title/ranks based on Pokemon Gold/Silver and Team Rocket Ranks
---	Source referenced is Full Trainer Info List - Guide and Walkthrough (GBC) by Eevee-Trainer / _Cecilia_ v1.13
---  Source URL: https://gamefaqs.gamespot.com/gbc/446340-pokemon-silver-version/faqs/75211/full-trainer-info-list
---  Source referenced is Ranks on Team Rocket Fandom
---  Source URL: https://teamrocket.fandom.com/wiki/Ranks
-
-INSERT INTO Pokemon (pokeName, isBaby, pokeRarity)
-VALUES 
+INSERT INTO
+	Pokemon (pokeName, isBaby, pokeRarity)
+VALUES
 	("Pikachu", 0, "Common"),
 	("Charizard", 0, "Rare"),
 	("Budew", 1, "Common"),
 	("Mewtwo", 0, "Legendary"),
-	("Arceus", 0, "Mythical"); 
+	("Arceus", 0, "Mythical");
 
-INSERT INTO Affiliations (affiliationType, affiliationRank, typeDescription)
-VALUES 
-	("Team Rocket", "Agent", "Villanious organization"), 
-	("Trainer", "Gym Leader", "Elite trainers"), 
+INSERT INTO
+	Affiliations (
+		affiliationType,
+		affiliationRank,
+		typeDescription
+	)
+VALUES
+	(
+		"Team Rocket",
+		"Agent",
+		"Villanious organization"
+	),
+	("Trainer", "Gym Leader", "Elite trainers"),
 	("Trainer", "Apprentice", "Standard trainers");
 
-INSERT INTO People (peopleName, affiliationID)
-VALUES 
-	("Jessie", (SELECT affiliationID
-				FROM Affiliations
-				WHERE affiliationType = "Team Rocket"
-				AND affiliationRank = "Agent")), 
-	("Misty", (SELECT affiliationID
-				FROM Affiliations
-				WHERE affiliationType = "Trainer"
-				AND affiliationRank = "Gym Leader")), 
-	("Ash", (SELECT affiliationID
-				FROM Affiliations
-				WHERE affiliationType = "Trainer"
-				AND affiliationRank = "Apprentice")); 
+INSERT INTO
+	People (peopleName, affiliationID)
+VALUES
+	(
+		"Jessie",
+		(
+			SELECT
+				affiliationID
+			FROM
+				Affiliations
+			WHERE
+				affiliationType = "Team Rocket"
+				AND affiliationRank = "Agent"
+		)
+	),
+	(
+		"Misty",
+		(
+			SELECT
+				affiliationID
+			FROM
+				Affiliations
+			WHERE
+				affiliationType = "Trainer"
+				AND affiliationRank = "Gym Leader"
+		)
+	),
+	(
+		"Ash",
+		(
+			SELECT
+				affiliationID
+			FROM
+				Affiliations
+			WHERE
+				affiliationType = "Trainer"
+				AND affiliationRank = "Apprentice"
+		)
+	);
 
-INSERT INTO Types (typeName, weakAgainst, strongAgainst)
-VALUES 
-	("Fire", "Water", "Grass"), 
-	("Water", "Electric", "Fire"), 
+INSERT INTO
+	Types (typeName, weakAgainst, strongAgainst)
+VALUES
+	("Fire", "Water", "Grass"),
+	("Water", "Electric", "Fire"),
 	("Grass", "Fire", "Water"),
-	("Ghost", "Dark", "Psychic"), 
-	("Electric", "Ground", "Water"), 
-	("Psychic", "Bug", "Poison"), 
+	("Ghost", "Dark", "Psychic"),
+	("Electric", "Ground", "Water"),
+	("Psychic", "Bug", "Poison"),
 	("Normal", "Rock", "Ghost");
 
-INSERT INTO Battles (battleDate, battleSetting)
-VALUES 
-	('2024-10-10', "Forest"), 
-	('2024-10-11', "Gym"), 
+INSERT INTO
+	Battles (battleDate, battleSetting)
+VALUES
+	('2024-10-10', "Forest"),
+	('2024-10-11', "Gym"),
 	('2024-10-12', "Cave");
 
-INSERT INTO Pokemon_Types (pokeID, typeID)
-VALUES 
-	((SELECT pokeID FROM Pokemon WHERE pokeName = "Pikachu"), (SELECT typeID FROM Types WHERE typeName = "Electric")), 
-	((SELECT pokeID FROM Pokemon WHERE pokeName = "Charizard"), (SELECT typeID FROM Types WHERE typeName = "Fire")), 
-	((SELECT pokeID FROM Pokemon WHERE pokeName = "Budew"), (SELECT typeID FROM Types WHERE typeName = "Grass")),
-	((SELECT pokeID FROM Pokemon WHERE pokeName = "Mewtwo"), (SELECT typeID FROM Types WHERE typeName = "Psychic")),
-	((SELECT pokeID FROM Pokemon WHERE pokeName = "Arceus"), (SELECT typeID FROM Types WHERE typeName = "Normal"));
+INSERT INTO
+	Pokemon_Types (pokeID, typeID)
+VALUES
+	(
+		(
+			SELECT
+				pokeID
+			FROM
+				Pokemon
+			WHERE
+				pokeName = "Pikachu"
+		),
+		(
+			SELECT
+				typeID
+			FROM
+				Types
+			WHERE
+				typeName = "Electric"
+		)
+	),
+	(
+		(
+			SELECT
+				pokeID
+			FROM
+				Pokemon
+			WHERE
+				pokeName = "Charizard"
+		),
+		(
+			SELECT
+				typeID
+			FROM
+				Types
+			WHERE
+				typeName = "Fire"
+		)
+	),
+	(
+		(
+			SELECT
+				pokeID
+			FROM
+				Pokemon
+			WHERE
+				pokeName = "Budew"
+		),
+		(
+			SELECT
+				typeID
+			FROM
+				Types
+			WHERE
+				typeName = "Grass"
+		)
+	),
+	(
+		(
+			SELECT
+				pokeID
+			FROM
+				Pokemon
+			WHERE
+				pokeName = "Mewtwo"
+		),
+		(
+			SELECT
+				typeID
+			FROM
+				Types
+			WHERE
+				typeName = "Psychic"
+		)
+	),
+	(
+		(
+			SELECT
+				pokeID
+			FROM
+				Pokemon
+			WHERE
+				pokeName = "Arceus"
+		),
+		(
+			SELECT
+				typeID
+			FROM
+				Types
+			WHERE
+				typeName = "Normal"
+		)
+	);
 
-INSERT INTO Pokemon_People (pokeID, peopleID, pokeNickname, caughtDate, caughtAt)
-VALUES 
-	((SELECT pokeID FROM Pokemon WHERE pokeName = "Pikachu"), (SELECT peopleID FROM People WHERE peopleName = "Ash"), NULL, '2024-09-07', "Pallet Town"),
-	((SELECT pokeID FROM Pokemon WHERE pokeName = "Charizard"), (SELECT peopleID FROM People WHERE peopleName = "Misty"), NULL, '2024-07-01', "Cerulean City"),
-	((SELECT pokeID FROM Pokemon WHERE pokeName = "Budew"), (SELECT peopleID FROM People WHERE peopleName = "Jessie"), "Buddy", '2024-09-21', "Pinwheel Forest");
+INSERT INTO
+	Pokemon_People (
+		pokeID,
+		peopleID,
+		pokeNickname,
+		caughtDate,
+		caughtAt
+	)
+VALUES
+	(
+		(
+			SELECT
+				pokeID
+			FROM
+				Pokemon
+			WHERE
+				pokeName = "Pikachu"
+		),
+		(
+			SELECT
+				peopleID
+			FROM
+				People
+			WHERE
+				peopleName = "Ash"
+		),
+		NULL,
+		'2024-09-07',
+		"Pallet Town"
+	),
+	(
+		(
+			SELECT
+				pokeID
+			FROM
+				Pokemon
+			WHERE
+				pokeName = "Charizard"
+		),
+		(
+			SELECT
+				peopleID
+			FROM
+				People
+			WHERE
+				peopleName = "Misty"
+		),
+		NULL,
+		'2024-07-01',
+		"Cerulean City"
+	),
+	(
+		(
+			SELECT
+				pokeID
+			FROM
+				Pokemon
+			WHERE
+				pokeName = "Budew"
+		),
+		(
+			SELECT
+				peopleID
+			FROM
+				People
+			WHERE
+				peopleName = "Jessie"
+		),
+		"Buddy",
+		'2024-09-21',
+		"Pinwheel Forest"
+	);
 
-INSERT INTO People_Battles (peopleID, battleID, battleResult)
-VALUES 
-	(1, 1, "Won"), 
-	(2, 1, "Lost"), 
-	(1, 2, "Lost"), 
+INSERT INTO
+	People_Battles (peopleID, battleID, battleResult)
+VALUES
+	(1, 1, "Won"),
+	(2, 1, "Lost"),
+	(1, 2, "Lost"),
 	(3, 2, "Won");
-			
-SET FOREIGN_KEY_CHECKS=1;
+
+SET
+	FOREIGN_KEY_CHECKS = 1;
+
 COMMIT;
--- DML.sql
+++ DML.sql
@@ -1,226 +1,351 @@
 ---------------------------------------------------------------
 -- Data Definition Queries for the Pokemon Management System --
--- Team members: Elena Parapounsky & Amy Xu  --
 ---------------------------------------------------------------
+SET
+    FOREIGN_KEY_CHECKS = 0;
 
-/* Citation for setting foreign key checks/autocommits:
- Date: 11/05/2024
- Based on recommendation to disable commits and foreign checks from CS340 - Project Step 2 Draft: Normalized Schema + DDL with Sample Data
- Source URL: https://canvas.oregonstate.edu/courses/1976520/assignments/9783693?module_item_id=24719057
-*/
-
-SET FOREIGN_KEY_CHECKS=0;
-SET AUTOCOMMIT = 0;
-
-/* Citation for the following function(s):
- Date: 11/05/2024
- Based on CRUD table function from CS340 - Exploration: Database Application Design
- Source URL: https://canvas.oregonstate.edu/courses/1976520/pages/exploration-database-application-design?module_item_id=24719062
-*/
-
+SET
+    AUTOCOMMIT = 0;
 
 ---------------------------------------------------------------
 --               CRUD operations for Entity Tables           --
 ---------------------------------------------------------------
-
 --                    Pokemon Table                          --
 ---------------------------------------------------------------
 -- Create
-INSERT INTO Pokemon (pokeName, isBaby, pokeRarity)
-VALUES (:pokeNameInput, :isBabySelect, :pokeRaritySelect); 
+INSERT INTO
+    Pokemon (pokeName, isBaby, pokeRarity)
+VALUES
+    (:pokeNameInput, :isBabySelect, :pokeRaritySelect);
 
 -- Read
-SELECT pokeID AS "Pokemon ID", pokeName AS "Pokemon Name", isBaby AS "Is Baby?", pokeRarity AS "Pokemon Rarity"
-FROM Pokemon;
+SELECT
+    pokeID AS "Pokemon ID",
+    pokeName AS "Pokemon Name",
+    isBaby AS "Is Baby?",
+    pokeRarity AS "Pokemon Rarity"
+FROM
+    Pokemon;
 
 -- Update
-UPDATE Pokemon
-SET pokeName = :pokeNameInput, isBaby = :isBabySelect, pokeRarity = :pokeRaritySelect
-WHERE pokeID =  :pokeID_selected_from_browse_Pokemon_page;
+UPDATE
+    Pokemon
+SET
+    pokeName = :pokeNameInput,
+    isBaby = :isBabySelect,
+    pokeRarity = :pokeRaritySelect
+WHERE
+    pokeID = :pokeID_selected_from_browse_Pokemon_page;
 
 -- Delete
-DELETE 
-FROM Pokemon 
-WHERE pokeID = :pokeID_selected_from_browse_Pokemon_page;
-
+DELETE FROM
+    Pokemon
+WHERE
+    pokeID = :pokeID_selected_from_browse_Pokemon_page;
 
 --                    People Table                          --
 ---------------------------------------------------------------
 -- Create
 --      get affialitionID and "Type Rank" affiliations for Affiliation dropdown
-SELECT affiliationID, CONCAT(affilationType,' ',affiliationRank) AS affiliation
-FROM Affiliations;
+SELECT
+    affiliationID,
+    CONCAT(affilationType, ' ', affiliationRank) AS affiliation
+FROM
+    Affiliations;
 
-INSERT INTO People (peopleName, affiliation)
-VALUES (:peopleNameInput, :affiliation_from_dropdown_Input); 
+INSERT INTO
+    People (peopleName, affiliation)
+VALUES
+    (
+        :peopleNameInput,
+        :affiliation_from_dropdown_Input
+    );
 
 -- Read
-SELECT peopleID AS "Person ID", peopleName AS "Person Name", CONCAT(Affiliations.affiliationType,' ',Affiliations.affiliationRank) AS "Person Affiliation"
-FROM People
-INNER JOIN Affiliations ON People.affiliationID = Affiliations.affiliationID;
+SELECT
+    peopleID AS "Person ID",
+    peopleName AS "Person Name",
+    CONCAT(
+        Affiliations.affiliationType,
+        ' ',
+        Affiliations.affiliationRank
+    ) AS "Person Affiliation"
+FROM
+    People
+    INNER JOIN Affiliations ON People.affiliationID = Affiliations.affiliationID;
 
 -- Update
-UPDATE People
-SET peopleName = :peopleNameInput, Affiliation = :affiliationID_from_dropdown_Input
-WHERE peopleID =  :peopleID_selected_from_browse_People_page;
+UPDATE
+    People
+SET
+    peopleName = :peopleNameInput,
+    Affiliation = :affiliationID_from_dropdown_Input
+WHERE
+    peopleID = :peopleID_selected_from_browse_People_page;
 
 -- Delete
-DELETE 
-FROM People
-WHERE peopleID = :peopleID_selected_from_browse_People_page;
-
+DELETE FROM
+    People
+WHERE
+    peopleID = :peopleID_selected_from_browse_People_page;
 
 --                    Affiliations Table                          --
 ---------------------------------------------------------------
 -- Create
-INSERT INTO Affiliations (affiliationType, affiliationRank, typeDescription)
-VALUES (:affiliationTypeInput, :affiliationRankInput, :typeDescriptionInput); 
+INSERT INTO
+    Affiliations (
+        affiliationType,
+        affiliationRank,
+        typeDescription
+    )
+VALUES
+    (
+        :affiliationTypeInput,
+        :affiliationRankInput,
+        :typeDescriptionInput
+    );
 
 -- Read
-SELECT affiliationID AS "Affiliation ID", affiliationType AS "Affiliation Type", affiliationRank AS "Affiliation Rank", typeDescription AS "Type Description"
-FROM Affiliations;
+SELECT
+    affiliationID AS "Affiliation ID",
+    affiliationType AS "Affiliation Type",
+    affiliationRank AS "Affiliation Rank",
+    typeDescription AS "Type Description"
+FROM
+    Affiliations;
 
 -- Update
-UPDATE Affiliations
-SET affiliationType = :affiliationTypeInput, affiliationRank = :affiliationRankInput, typeDescription = :typeDescriptionInput
-WHERE affliationID =  :affiliationID_selected_from_browse_Affiliations_page;
+UPDATE
+    Affiliations
+SET
+    affiliationType = :affiliationTypeInput,
+    affiliationRank = :affiliationRankInput,
+    typeDescription = :typeDescriptionInput
+WHERE
+    affliationID = :affiliationID_selected_from_browse_Affiliations_page;
 
 -- Delete
-DELETE 
-FROM Affiliations
-WHERE affiliationID = :affiliationID_selected_from_browse_Affiliations_page;
-
+DELETE FROM
+    Affiliations
+WHERE
+    affiliationID = :affiliationID_selected_from_browse_Affiliations_page;
 
 --                      Types Table                          --
 ---------------------------------------------------------------
 -- Create
-INSERT INTO Types (typeName, weakAgainst, strongAgainst)
-VALUES (:typeNameInput, :weakAgainstInput, :strongAgainstInput); 
+INSERT INTO
+    Types (typeName, weakAgainst, strongAgainst)
+VALUES
+    (
+        :typeNameInput,
+        :weakAgainstInput,
+        :strongAgainstInput
+    );
 
 -- Read
-SELECT typeID AS "Type ID", typeName AS "Type Name", weakAgainst AS "Weak Against", strongAgainst AS "Strong Against"
-FROM Types;
+SELECT
+    typeID AS "Type ID",
+    typeName AS "Type Name",
+    weakAgainst AS "Weak Against",
+    strongAgainst AS "Strong Against"
+FROM
+    Types;
 
 -- Update
-UPDATE Types
-SET typeName = :typeNameInput, weakAgainst = :weakAgainstInput, strongAgainst = :strongAgainstInput
-WHERE typeID =  :typeID_selected_from_browse_Types_page;
+UPDATE
+    Types
+SET
+    typeName = :typeNameInput,
+    weakAgainst = :weakAgainstInput,
+    strongAgainst = :strongAgainstInput
+WHERE
+    typeID = :typeID_selected_from_browse_Types_page;
 
 -- Delete
-DELETE 
-FROM Types
-WHERE typeID = :typeID_selected_from_browse_Types_page;
+DELETE FROM
+    Types
+WHERE
+    typeID = :typeID_selected_from_browse_Types_page;
 
 --                      Battles Table                        --
 ---------------------------------------------------------------
 -- Create
-INSERT INTO Battles (battleDate, battleSetting)
-VALUES (:battleDateInput, :battleSettingInput); 
+INSERT INTO
+    Battles (battleDate, battleSetting)
+VALUES
+    (:battleDateInput, :battleSettingInput);
 
 -- Read
-SELECT battleID AS "Battle ID", battleDate AS "Battle Date", battleSetting AS "Battle Setting"
-FROM Battles;
+SELECT
+    battleID AS "Battle ID",
+    battleDate AS "Battle Date",
+    battleSetting AS "Battle Setting"
+FROM
+    Battles;
 
 -- Update
-UPDATE Battles
-SET battleDate = :battleDateInput, battleSetting = :battleSettingInput
-WHERE battleID =  :battleID_selected_from_browse_Battles_page;
+UPDATE
+    Battles
+SET
+    battleDate = :battleDateInput,
+    battleSetting = :battleSettingInput
+WHERE
+    battleID = :battleID_selected_from_browse_Battles_page;
 
 -- Delete
-DELETE 
-FROM Battles
-WHERE battleID = :battleID_selected_from_browse_Battles_page;
+DELETE FROM
+    Battles
+WHERE
+    battleID = :battleID_selected_from_browse_Battles_page;
 
 ---------------------------------------------------------------
 --         CRUD operations for Intersecting Tables           --
 ---------------------------------------------------------------
-
 --                 Pokemon_People Table                      --
 ---------------------------------------------------------------
 -- Create
 --      get pokeID and pokeName for Pokemon dropdown
-SELECT pokeID, pokeName
-FROM Pokemon;
+SELECT
+    pokeID,
+    pokeName
+FROM
+    Pokemon;
 
 --      get peopleID and peopleName for Owner dropdown
-SELECT peopleID, peopleName
-FROM People;
+SELECT
+    peopleID,
+    peopleName
+FROM
+    People;
 
-INSERT INTO Pokemon_People (Pokemon, Owner, pokeNickname, caughtDate, caughtAt)
-VALUES (:pokeID_from_dropdown_Input, :peopleID_from_dropdown_Input, :pokeNicknameInput, :caughtDateInput, :caughtAtInput); 
+INSERT INTO
+    Pokemon_People (
+        Pokemon,
+        Owner,
+        pokeNickname,
+        caughtDate,
+        caughtAt
+    )
+VALUES
+    (
+        :pokeID_from_dropdown_Input,
+        :peopleID_from_dropdown_Input,
+        :pokeNicknameInput,
+        :caughtDateInput,
+        :caughtAtInput
+    );
 
 -- Read
-SELECT Pokemon.pokeName AS Pokemon, People.peopleName AS Owner, pokeNickname AS "Pokemon Nickname", caughtDate AS "Caught Date", caughtAt AS "Caught Location"
-FROM Pokemon_People
-INNER JOIN Pokemon ON Pokemon_People.pokeID = Pokemon.pokeID
-INNER JOIN People ON Pokemon_People.peopleID = People.peopleID;
+SELECT
+    Pokemon.pokeName AS Pokemon,
+    People.peopleName AS Owner,
+    pokeNickname AS "Pokemon Nickname",
+    caughtDate AS "Caught Date",
+    caughtAt AS "Caught Location"
+FROM
+    Pokemon_People
+    INNER JOIN Pokemon ON Pokemon_People.pokeID = Pokemon.pokeID
+    INNER JOIN People ON Pokemon_People.peopleID = People.peopleID;
 
 -- Update
-UPDATE Pokemon_People
-SET pokeNickname = :pokeNicknameInput, caughtDate = :caughtDateInput, caughtAt = :caughtAtInput
-WHERE pokeID =  :pokeID_selected_from_browse_Pokemon_People_page
-AND peopleID =  :peopleID_selected_from_browse_Pokemon_People_page;
+UPDATE
+    Pokemon_People
+SET
+    pokeNickname = :pokeNicknameInput,
+    caughtDate = :caughtDateInput,
+    caughtAt = :caughtAtInput
+WHERE
+    pokeID = :pokeID_selected_from_browse_Pokemon_People_page
+    AND peopleID = :peopleID_selected_from_browse_Pokemon_People_page;
 
 -- Delete
-DELETE 
-FROM Pokemon_People
-WHERE pokeID =  :pokeID_selected_from_browse_Pokemon_People_page
-AND peopleID =  :peopleID_selected_from_browse_Pokemon_People_page;
-
+DELETE FROM
+    Pokemon_People
+WHERE
+    pokeID = :pokeID_selected_from_browse_Pokemon_People_page
+    AND peopleID = :peopleID_selected_from_browse_Pokemon_People_page;
 
 --                  Pokemon_Types Table                      --
 ---------------------------------------------------------------
 -- Create
 --      get pokeID and pokeName for Pokemon dropdown
-SELECT pokeID, pokeName
-FROM Pokemon;
+SELECT
+    pokeID,
+    pokeName
+FROM
+    Pokemon;
 
 --      get typeID and typeName for Type dropdown
-SELECT typeID, typeName
-FROM Types;
+SELECT
+    typeID,
+    typeName
+FROM
+    Types;
 
-INSERT INTO Pokemon_Types (Pokemon, Type)
-VALUES (:pokeID_from_dropdown_Input, :typeID_from_dropdown_Input); 
+INSERT INTO
+    Pokemon_Types (Pokemon, Type)
+VALUES
+    (
+        :pokeID_from_dropdown_Input,
+        :typeID_from_dropdown_Input
+    );
 
 -- Read
-SELECT Pokemon.pokeName AS Pokemon, Types.typeName AS Type
-FROM Pokemon_Types
-INNER JOIN Pokemon ON Pokemon_Types.pokeID = Pokemon.pokeID
-INNER JOIN Types ON Pokemon_Types.typeID = Types.typeID;
+SELECT
+    Pokemon.pokeName AS Pokemon,
+    Types.typeName AS Type
+FROM
+    Pokemon_Types
+    INNER JOIN Pokemon ON Pokemon_Types.pokeID = Pokemon.pokeID
+    INNER JOIN Types ON Pokemon_Types.typeID = Types.typeID;
 
 -- Update
 -- Unable to update as in intersecting table with no other attributes other than Primary keys
-
-
 -- Delete
-DELETE 
-FROM Pokemon_Types
-WHERE pokeID =  :pokeID_selected_from_browse_Pokemon_Types_page
-AND typeID =  :typeID_selected_from_browse_Pokemon_Types_page;
-
+DELETE FROM
+    Pokemon_Types
+WHERE
+    pokeID = :pokeID_selected_from_browse_Pokemon_Types_page
+    AND typeID = :typeID_selected_from_browse_Pokemon_Types_page;
 
 --                 People_Battles Table                      --
 ---------------------------------------------------------------
 -- Create
-INSERT INTO People_Battles (PersonID, battleID, battleResult)
-VALUES (:personIDInput, :battleIDInput, :battleResultSelect); 
+INSERT INTO
+    People_Battles (PersonID, battleID, battleResult)
+VALUES
+    (
+        :personIDInput,
+        :battleIDInput,
+        :battleResultSelect
+    );
 
 -- Read
-SELECT peopleID AS "Person ID", battleID AS "Battle ID", battleResult AS "Battle Result"
-FROM People_Battles;
+SELECT
+    peopleID AS "Person ID",
+    battleID AS "Battle ID",
+    battleResult AS "Battle Result"
+FROM
+    People_Battles;
 
 -- Update
-UPDATE People_Battles
-SET battleResult = :battleResultSelect;
-WHERE peopleID =  :peopleID_selected_from_browse_People_Battles_page
-AND battleID =  :battleID_selected_from_browse_People_Battles_page;
+UPDATE
+    People_Battles
+SET
+    battleResult = :battleResultSelect;
+
+WHERE
+    peopleID = :peopleID_selected_from_browse_People_Battles_page
+    AND battleID = :battleID_selected_from_browse_People_Battles_page;
 
 -- Delete
-DELETE 
-FROM People_Battles
-WHERE peopleID =  :peopleID_selected_from_browse_People_Battles_page
-AND battleID =  :battleID_selected_from_browse_People_Battles_page;
+DELETE FROM
+    People_Battles
+WHERE
+    peopleID = :peopleID_selected_from_browse_People_Battles_page
+    AND battleID = :battleID_selected_from_browse_People_Battles_page;
 
+SET
+    FOREIGN_KEY_CHECKS = 1;
 
-SET FOREIGN_KEY_CHECKS=1;
 COMMIT;
